3b67dad807a3fcc27e21e9066ce0d0f1e28aaece
Sebastian Essink <sessink@users.noreply.github.com>
2019-09-19 12:12:44 -0700

    mad

--- notebooks/epsilon_tools.py
+++ notebooks/epsilon_tools.py
@@ -65,10 +65,11 @@ def H2ADCfun(Hz):
     ''' H2 ADC transfer function
     '''
     import numpy as np
+    import math
     Fc5 = 120
     Fc3 = 210  # in Hz
-    sinc5 = np.sin(np.pi * Hz / Fc5) / (np.pi * Hz / Fc5)
-    sinc3 = np.sin(np.pi * Hz / Fc3) / (np.pi * Hz / Fc3)
+    sinc5 = np.sin(math.pi * Hz / Fc5) / (math.pi * Hz / Fc5)
+    sinc3 = np.sin(math.pi * Hz / Fc3) / (math.pi * Hz / Fc3)
     H = (sinc5**5) * (sinc3**3)
     return H**2
 
@@ -79,51 +80,32 @@ def H2FP07fun(Hz, w):
         Hz is frequency in Hz
         U is velocity in m/s
     '''
-    import numpy as np
+    import math
 #     gamma = -0.5 # Hill, 1987
     gamma = -0.32 # Gregg&Meager, 1980
     tau0 = 0.005 # [ms] Gregg&Meager, 1980
     tau = tau0 * w**gamma
-    return (1 + (2 * np.pi * Hz * tau)**2)**(-2)
+    return (1 + (2 * math.pi * Hz * tau)**2)**(-2)
 
 
 def H2preampfun(Hz):
     ''' H2 Preamp transfer function
     '''
-    import numpy as np
+    import math
     Fc1 = 339
     Fc2 = 339
     Gd = 0.965
     # in Hz
     H2_1 = (1 - (Hz**2) / Fc1 / Fc2)**2
-    H2_2 = (Hz / Fc1 + Hz / Fc2 + 2 * np.pi * Hz * Gd)**2
+    H2_2 = (Hz / Fc1 + Hz / Fc2 + 2 * math.pi * Hz * Gd)**2
     H2_3 = (1 + (Hz / Fc1)**2) * (1 + (Hz / Fc2)**2)
     return H2_1 + H2_2 / H2_3
-
-
-
-
-def kraichnan(k_rpm, chi, kb_rpm):
-    '''
-    Kraichnan temperature gradient spectrum
-
-    adapted from: Goto et al., 2016
-    '''
-    import numpy as np
-    import math
-    D = 1.4e-7
-    nu = 1.2e-6
-    qk = 5.27
-    
-    yk = math.sqrt(qk)* k_rpm / kb_rpm
-    nom = chi*math.sqrt(qk)*yk**2*np.exp(-math.sqrt(6)*yk)
-    denom = (D*kb_rpm*yk)
-    return nom/denom
 
 
 def noise_sp(f_cps):
     # noisesp = 1.0e-11 * [1+(f/130)**3]**2
     return 1e-11 * (1 + (f_cps / 20)**3)**2
+
 
 def remove_noise_sp(tms, threshold):
     '''
@@ -137,23 +119,44 @@ def remove_noise_sp(tms, threshold):
         tms.corrTsp2_cps / (threshold * noisesp) > 1, 0)
     return tms
 
-def batchelor(k_rpm, chi, kb_rpm):
-    '''
-    Batchelor temperature gradient spectrum
 
-    reference: Oakey, 1982
-    adapted from: RC Lien, 1992
+def batchelor(k_rpm, chi, kb_rpm, p):
+    ''' wrapper for batchelor spectrum function to apply to xr dataarray
     '''
-    import numpy as np
-    import math
-    from scipy.special import erfc
-    D = 1.4e-7
-    nu = 1.2e-6
-    q = 3.7
+    def np_batchelor(k_rpm, chi, kb_rpm, p):
+        '''
+        Batchelor temperature gradient spectrum
+
+        reference: Oakey, 1982
+        see also: Lien, 1992
+        '''
+        import numpy as np
+        import math
+        from scipy.special import erfc
+
+        a = np.sqrt(2 * p.q) * k_rpm / kb_rpm
+        uppera = []
+        for ai in a:
+            uppera.append(erfc(ai / math.sqrt(2)) * math.sqrt(0.5 * math.pi))
+        g = 2 * math.pi * a * (np.exp(-0.5 * a**2) - a * np.array(uppera))
+        return math.sqrt(0.5 * p.q) * (chi / (kb_rpm* p.D)) * g / (2 * math.pi)
     
-    a = np.sqrt(2 * q) * k_rpm / kb_rpm
-    uppera = []
-    for ai in a:
-        uppera.append(erfc(ai / math.sqrt(2)) * math.sqrt(0.5 * math.pi))
-    g = 2 * math.pi * a * (np.exp(-0.5 * a**2) - a * np.array(uppera))
-    return math.sqrt(0.5 * q) * (chi / (kb_rpm * D)) * g / (2 * math.pi)
+    return xr.apply_ufunc(np_batchelor, k_rpm, chi, kb_rpm, p)
+
+
+def kraichnan(k_rpm, chi, kb_rpm, p):
+    ''' wrapper for kraichnan spectrum function to apply to xr dataarray
+    '''
+    def np_kraichnan(k_rpm, chi, kb_rpm, p):
+        '''
+        Kraichnan temperature gradient spectrum
+
+        adapted from: Goto et al., 2016
+        '''
+        import numpy as np
+        import math
+        yk = math.sqrt(p.qk)* k_rpm / kb_rpm
+        nom = chi*math.sqrt(p.qk)*yk*np.exp(-math.sqrt(6)*yk)
+        denom = (p.D*kb_rpm)
+        return nom/denom
+    return xr.apply_ufunc(np_kraichnan, k_rpm, chi, kb_rpm, p)
