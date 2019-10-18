03ebab8922a9be80e36547cd6a09b6cdb6456eb4
Sebastian Essink <sessink@users.noreply.github.com>
2019-10-18 16:21:14 -0700

    fix snakefile

--- Snakefile
+++ Snakefile
@@ -1,8 +1,19 @@
+path = 'data/niw2016/'
+
+#FLOATS: ['7779a', '7781a', '7783a', '7786a', '7787a', '7788a',
+#    '7700b', '7701b','7780b', '7784b', '7785b', '7786b']
+
+FLOATS: ['7779a']
+
+
+rule all:
+    input:
+        expand(path+'{float}/ema-{float}{wildcards}-ctd.nc', float=FLOATS)
+
 rule compute_eps:
     input:
-        'data/NIWmatdata/{wildcard}_grid.mat',
-        'data/NIWmatdata/{wildcard}_grid.mat',
+        path+'{float}/ema-{float}{wildcards}-ctd.mat'
     output:
-        'data/NIWmatdata/{wildcard}_grid.nc'
+        path+'{float}/ema-{float}{wildcards}-ctd.nc'
     script:
-        'scipts/compute_epsilon.py'
+        'scipts/test_script.py'
--- scripts/compute_epsilon.py
+++ scripts/compute_epsilon.py
@@ -1,2 +1,63 @@
-print(snakemake.input[0])
-print(snakemake.input[1])
+import sys
+sys.path.append('../scripts/')
+
+from time import time
+import glob
+import warnings
+import param
+
+import numpy as np
+import pandas as pd
+import xarray as xr
+from scipy.special import gammaln
+from scipy.optimize import curve_fit, minimize
+
+# Plotting
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+import hvplot.xarray
+
+from epsilon_tools import *
+
+import warnings
+warnings.simplefilter("ignore",category=FutureWarning)
+warnings.simplefilter("ignore",category=RuntimeWarning)
+
+# set up figure params
+sns.set(style='ticks', context='paper', palette='colorblind')
+mpl.rc('figure', dpi=100, figsize=[11, 5])
+mpl.rc('savefig', dpi=500, bbox='tight')
+mpl.rc('legend', frameon=False)
+
+# %% MAIN
+p = Parameters()
+a = time()
+liste = ['7786b-0200']
+ds=[]
+for l in liste:
+    chi_dir = '../data/test_profiles/ema-'+l+'-tms.mat'
+    tms = convert_tmsdata(chi_dir)
+    ctd_dir = '../data/test_profiles/ema-'+l+'-ctd.mat'
+    ctd = convert_ctddata(ctd_dir)
+
+    turb = []
+    for jblock in range(tms.time.size):
+
+        tms_block = tms.isel(time=jblock)
+        tms_block = prepare_data(tms_block, ctd)
+        tms_block = compute_chi(tms_block, p)
+        tms_block = compute_rc_eps(tms_block, p)
+        tms_block = compute_goto_eps(tms_block, p)
+
+        tms_block = tms_block.swap_dims({'k_rpm': 'f_cps'})
+        turb.append(tms_block)
+
+    turb = xr.concat(turb, dim='time')
+    ds.append(turb)
+
+ds =  xr.concat(ds, dim='time')
+ds['dof'] = ds.dof.isel(time=0)
+
+b = (time()-a)/60
+print(f'{b:2.2f} minutes')
